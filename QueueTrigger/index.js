75ce1d7931250aa832c109af43cc8146339fc4ac
Ajinkya <ajinkya599@gmail.com>
2019-04-24 18:15:53 +0530

    Update index.js

--- QueueTrigger/index.js
+++ QueueTrigger/index.js
@@ -1,4 +1,4 @@
 module.exports = async function (context, myQueueItem) {
-    context.log('JavaScript queue trigger function processed work item', myQueueItem);
+    context.log('JavaScript queue trigger function 2.0 processed work item', myQueueItem);
 };
 process.stdout.write("hello: ");
